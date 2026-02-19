262ba8cbba9564879905bff3e66f0584f897ddf1
sohammanna275 <soham.manna275@gmail.com>
2026-02-19 15:39:50 +0530

    initial update 2

--- src/App.jsx
+++ src/App.jsx
@@ -722,11 +722,11 @@ export default function App() {
       <ConferenceDetails />
       <VenueSection />
       <AddressSection />
-      <PartnerGrid title="In Association with" partners={associationData} />
-      <PartnerGrid title="Sponsored by" partners={sponsorData} />
+      {/* <PartnerGrid title="In Association with" partners={associationData} /> */}
+      {/* <PartnerGrid title="Sponsored by" partners={sponsorData} /> */}
       <AboutIIESTSection />
-      <ConferenceInfoSection />
-      <TimelineSection />
+      {/* <ConferenceInfoSection /> */}
+      {/* <TimelineSection /> */}
       <Footer />
       <div className="max-w-5xl mx-auto py-20 px-6">
 
