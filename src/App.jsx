f5c8e18ee6db3b746d25d49ffc424c9373e6d201
sohammanna275 <soham.manna275@gmail.com>
2026-05-19 17:43:58 +0530

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -757,7 +757,6 @@ const SubmitPaperPage = () => {
           Submit Your Paper →
         </a>
         <p>
-          *
           The Microsoft CMT service was used for managing the peer-reviewing process for this conference. This service was provided for free by Microsoft and they bore all expenses, including costs for Azure cloud services as well as for software development and support.
         </p>
       </div>
